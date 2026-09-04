14b9b069489533bec325e7f526f2b05b2f4aba1c
DERIN DENNY MATHEW <derindenny65@gmail.com>
2026-09-04 19:16:57 +0530

    Fix syntax error in next.config.mjs

--- next.config.mjs
+++ next.config.mjs
@@ -15,21 +15,4 @@ const nextConfig = {
   },
 };
 
-
-
-
 export default nextConfig;
-  reactStrictMode: true,
-  images: {
-    Standard_scalar: {
-      remotePatterns: [
-        {
-          protocol: 'https',
-          hostname: 'assets.coingecko.com',
-        },
-        {
-          protocol: 'https',
-          hostname: 'coin-images.coingecko.com',
-        },
-      ]}
-
